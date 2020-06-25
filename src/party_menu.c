130909c518ced3e97ede75116ea817910cc8e3e5
André Santos <contact.andre.santos@gmail.com>
2020-06-25 20:38:05 +0100

    [FEATURE] infinite use TMs

--- src/party_menu.c
+++ src/party_menu.c
@@ -4836,8 +4836,6 @@ static void Task_LearnedMove(u8 taskId)
     if (move[1] == 0)
     {
         AdjustFriendship(mon, 4);
-        if (item < ITEM_HM01_CUT)
-            RemoveBagItem(item, 1);
     }
     GetMonNickname(mon, gStringVar1);
     StringCopy(gStringVar2, gMoveNames[move[0]]);
